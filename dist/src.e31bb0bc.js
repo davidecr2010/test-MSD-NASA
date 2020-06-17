ac1b20db41e04d60047bdabadacaa9d3ec281364
casta <davidecr@gmail.com>
2020-06-16 21:32:25 -0500

    testing netlify

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -31953,7 +31953,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52809" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55476" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
